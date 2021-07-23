ec1445f052e9d3fc4eed7e61cc554e3dcab0e8b6
devendra-jit <85719346+sujit-bizcloudexperts@users.noreply.github.com>
2021-07-23 21:28:43 +0530

    batchinsert record and update status

--- src/RedshiftRecord/GetRedshiftRecord/index.js
+++ src/RedshiftRecord/GetRedshiftRecord/index.js
@@ -1,23 +1,86 @@
 const { Client } = require("pg");
+const Dynamo = require('../../shared/dynamo/db');
+const PROJECT44_TABLE = process.env.PROJECT44_TABLE;
+
 const client = new Client({
-  database: "prod_datamodel",
-  host: "omni-dw-prod.cxwdxqiiif5u.us-east-1.redshift.amazonaws.com",
-  port: 5439,
-  user: "bceuser1",
-  password: "BizCloudExp1",
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
-module.exports.handler = async (event) => {
-  console.log("started");
-  try {
-    const conn = await client.connect();
-    console.log("conn", conn);
-    const response = await client.query(
-      "select shipment_info.file_nbr ,shipment_info.house_bill_nbr ,shipment_info.bill_to_nbr ,shipment_info.origin_port_iata ,shipment_info.destination_port_iata , shipment_info.shipper_name ,shipment_info.shipper_addr_1 ,shipment_info.shipper_addr_2 ,shipment_info.shipper_city ,shipment_info.shipper_st , shipment_info.shipper_cntry ,shipment_info.shipper_zip , shipment_info.consignee_name ,shipment_info.consignee_addr_1 ,shipment_info.consignee_addr_2 ,shipment_info.consignee_city ,shipment_info.consignee_st , shipment_info.consignee_cntry ,shipment_info.consignee_zip , shipment_milestone.order_status ,shipment_milestone.order_status_desc ,shipment_milestone.region_code_basis ,shipment_Ref.ref_nbr from shipment_info left outer join shipment_milestone on shipment_info.source_system = shipment_milestone.source_system and shipment_info.file_nbr = shipment_milestone.file_nbr and is_custompublic = 'Y' left outer join (select source_system ,file_nbr ,ref_nbr from shipment_ref where ref_typeid = 'PO' and customer_type = 'B')shipment_Ref on shipment_info.source_system = shipment_Ref.source_system and shipment_info.file_nbr = shipment_Ref.file_nbr WHERE shipment_info.bill_to_nbr = '11912' and shipment_milestone.event_date >current_timestamp - interval '100 hour'"
-    );
-    console.log("response", response);
-  } catch (error) {
-    console.log("error : ", error);
+
+
+function compare(otherArray) {
+  return function (current) {
+    return otherArray.filter(function (other) {
+      return other.file_nbr == current.file_nbr && other.house_bill_nbr == current.house_bill_nbr && other.bill_to_nbr == current.bill_to_nbr && other.origin_port_iata == current.origin_port_iata && other.destination_port_iata == current.destination_port_iata && other.shipper_name == current.shipper_name && other.shipper_addr_1 == current.shipper_addr_1 && other.shipper_addr_2 == current.shipper_addr_2 && other.shipper_city == current.shipper_city && other.shipper_st == current.shipper_st && other.shipper_cntry == current.shipper_cntry && other.shipper_zip == current.shipper_zip && other.consignee_name == current.consignee_name && other.consignee_addr_1 == current.consignee_addr_1 && other.consignee_addr_2 == current.consignee_addr_2 && other.consignee_city == current.consignee_city && other.consignee_st == current.consignee_st && other.consignee_cntry == current.consignee_cntry && other.consignee_zip == current.consignee_zip && other.order_status == current.order_status && other.order_status_desc == current.order_status_desc && other.region_code_basis == current.region_code_basis && other.ref_nbr == current.ref_nbr
+    }).length == 0;
   }
+}
+
+function compareMatchRecord(otherArray) {
+  return function (current) {
+    return otherArray.filter(function (other) {
+      return other.file_nbr == current.file_nbr && other.house_bill_nbr == current.house_bill_nbr && other.bill_to_nbr == current.bill_to_nbr && other.origin_port_iata == current.origin_port_iata && other.destination_port_iata == current.destination_port_iata && other.shipper_name == current.shipper_name && other.shipper_addr_1 == current.shipper_addr_1 && other.shipper_addr_2 == current.shipper_addr_2 && other.shipper_city == current.shipper_city && other.shipper_st == current.shipper_st && other.shipper_cntry == current.shipper_cntry && other.shipper_zip == current.shipper_zip && other.consignee_name == current.consignee_name && other.consignee_addr_1 == current.consignee_addr_1 && other.consignee_addr_2 == current.consignee_addr_2 && other.consignee_city == current.consignee_city && other.consignee_st == current.consignee_st && other.consignee_cntry == current.consignee_cntry && other.consignee_zip == current.consignee_zip && other.order_status == current.order_status && other.order_status_desc == current.order_status_desc && other.region_code_basis == current.region_code_basis && other.ref_nbr == current.ref_nbr
+    }).length != 0;
+  }
+}
+
+
+module.exports.handler = async (event) => {
+  let response, dynamoData
+  try {
+  await client.connect();
+  response = await client.query(
+    "select shipment_info.file_nbr ,shipment_info.house_bill_nbr ,shipment_info.bill_to_nbr ,shipment_info.origin_port_iata ,shipment_info.destination_port_iata , shipment_info.shipper_name ,shipment_info.shipper_addr_1 ,shipment_info.shipper_addr_2 ,shipment_info.shipper_city ,shipment_info.shipper_st , shipment_info.shipper_cntry ,shipment_info.shipper_zip , shipment_info.consignee_name ,shipment_info.consignee_addr_1 ,shipment_info.consignee_addr_2 ,shipment_info.consignee_city ,shipment_info.consignee_st , shipment_info.consignee_cntry ,shipment_info.consignee_zip , shipment_milestone.order_status ,shipment_milestone.order_status_desc ,shipment_milestone.region_code_basis ,shipment_Ref.ref_nbr from shipment_info left outer join shipment_milestone on shipment_info.source_system = shipment_milestone.source_system and shipment_info.file_nbr = shipment_milestone.file_nbr and is_custompublic = 'Y' left outer join (select source_system ,file_nbr ,ref_nbr from shipment_ref where ref_typeid = 'PO' and customer_type = 'B')shipment_Ref on shipment_info.source_system = shipment_Ref.source_system and shipment_info.file_nbr = shipment_Ref.file_nbr WHERE shipment_info.bill_to_nbr = '11912' and shipment_milestone.event_date >current_timestamp - interval '1000 hour'"
+  );
   await client.end();
-  return event;
+  } catch (error) {
+    console.error("error : ", error);
+  }
+  console.info("response", response.rows);
+  try {
+    dynamoData = await Dynamo.getAllScanRecord(PROJECT44_TABLE);
+  } catch (e) {
+    console.error("error : ", e);
+  }
+  let notMatchedResult = (response.rows).filter(compare(dynamoData));
+  let matchedRecord = dynamoData.filter(compareMatchRecord(response.rows));
+  if (notMatchedResult.length) {
+    //send data project44 api
+    const status = 'success';
+    if (status == 'success') {
+      let inputRecord = []
+      notMatchedResult.map(element => {
+        element["notification_sent"] = "Y"
+        const data = {
+          "PutRequest": {
+            "Item": element
+          }
+        }
+        inputRecord.push(data)
+      })
+      try {
+        await Dynamo.batchInsertRecord(PROJECT44_TABLE, inputRecord);
+      } catch (e) {
+        console.error("Error : ", e);
+      }
+      if (matchedRecord.length) {
+        await updateRecord(matchedRecord);
+      }
+    } else {
+      console.error("Error : failure to send data in project44 api");
+    }
+  } else {
+    await updateRecord(matchedRecord);
+  }
 };
+
+async function updateRecord(arrayRecord) {
+  return arrayRecord.map(async (element) => {
+    if (element.notification_sent == "N") {
+      await Dynamo.updateItems(PROJECT44_TABLE, { file_nbr: element.file_nbr, order_status: element.order_status }, 'set notification_sent = :x', { ":x": "Y" });
+    }
+  })
+}
--- src/shared/dynamo/db.js
+++ src/shared/dynamo/db.js
@@ -275,7 +275,7 @@ async function getItemQueryWithLimit(tableName, limit, keyCondition, expressionA
     const documentClient = new AWS.DynamoDB.DocumentClient({ region: process.env.DEFAULT_AWS });
     const params = {
         TableName: tableName,
-        Limit: limit,   
+        Limit: limit,
         KeyConditionExpression: keyCondition,
         ExpressionAttributeValues: expressionAttribute
     };
@@ -307,6 +307,48 @@ async function getScanCount(tableName, filterExpression, expressionAttributeValu
     }
 }
 
+async function dbRead(params) {
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: process.env.DEFAULT_AWS });
+    let result = await documentClient.scan(params).promise();
+    let data = result.Items;
+    if (result.LastEvaluatedKey) {
+        params.ExclusiveStartKey = result.LastEvaluatedKey;
+        data = data.concat(await dbRead(params));
+    }
+    return data;
+}
+
+/* get all record scan */
+async function getAllScanRecord(tableName) {
+    const params = {
+        TableName: tableName
+    }
+    try {
+        return await dbRead(params);
+    } catch (e) {
+        console.error("getScanCount Error: ", e);
+        reject(handleError(1004, e, get(e, 'details[0].message', null)));
+    }
+}
+
+/* batch insert record */
+async function batchInsertRecord(tableName, records) {
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: process.env.DEFAULT_AWS });
+    const params = {
+        RequestItems: {
+            'omni-dw-project-44-devint': records
+        }
+    };
+    try {
+        return await documentClient.batchWrite(params).promise();
+    } catch (e) {
+        console.error("insert Error: ", e);
+        return await handleError(1007, e, get(e, 'details[0].message', null));
+    }
+}
+
+
+
 module.exports = {
     fetchAllItems,
     fetchByIndex,
@@ -322,5 +364,7 @@ module.exports = {
     checkApiKeyUsagePlan,
     apiKeyDelete,
     getItemQueryWithLimit,
-    getScanCount
+    getScanCount,
+    getAllScanRecord,
+    batchInsertRecord
 };
